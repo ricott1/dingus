61d2cab75526562b935609f509bf8cdca517d556
Alessandro Ricottone <ricott2@gmail.com>
2021-12-20 15:38:17 +0100

    clean up

--- dingus/transaction.py
+++ dingus/transaction.py
@@ -125,3 +125,24 @@ class BalanceTransfer(Transaction):
 
     def __init__(self, senderPublicKey : bytes, nonce:int, fee:int, asset: dict, strict:bool = False) -> None:
         super().__init__(senderPublicKey, nonce, fee, schemas.token_balance_transfer_asset.BalanceTransferAsset(), asset, strict)
+
+
+if __name__ == "__main__":
+    params = {
+        "senderPublicKey": utils.random_public_key().encode(),
+        "nonce": 0,
+        "fee": int(0.5 * LSK),
+        "asset": {
+            "amount": int(3 * LSK),
+            "recipientAddress": utils.random_address(),
+            "data": ""
+        }   
+    }
+
+    trs = BalanceTransfer.fromDict(params)
+    trs.nonce=166
+    
+    passphrase = "peanut hundred pen hawk invite exclude brain chunk gadget wait wrong ready"
+    trs.sign(utils.passphrase_to_sk(passphrase))
+    print(trs)
+    # trs.send()
