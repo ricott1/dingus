fab4bb991bf20836c794d2b7a1c1986d3702f479
Alessandro Ricottone <ricott2@gmail.com>
2022-01-03 16:17:00 +0100

    clean up __main__

--- dingus/__main__.py
+++ dingus/__main__.py
@@ -1,24 +1,13 @@
 from dingus.processor import Processor
-import dingus.utils as utils
-from dingus.types.keys import PrivateKey
 
 import logging
 import os       
+import argparse
 
 def start():
     logger = logging.getLogger(__name__)
     os.remove("dingus.log")
     logging.basicConfig(filename = "dingus.log", level=logging.DEBUG) 
-
-
-    # sk = utils.passphrase_to_sk("ale ale ale")
-    # # sk.to_json()
-    # print(sk.to_public_key().to_address())
-    # print(sk.to_public_key().to_address().to_avatar())
-
-
-
-    import argparse
 
     parser = argparse.ArgumentParser(description='Dingus arguments.')
     parser.add_argument(
