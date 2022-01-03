3371d81b6b04b094d609a92986b69983d4d103bd
Alessandro Ricottone <ricott2@gmail.com>
2022-01-03 16:05:22 +0100

    send LSK and use netowrk status update

---
+++ dingus/network/constants.py
@@ -0,0 +1,11 @@
+ENDPOINTS = {
+    "mainnet": "https://service.lisk.com",
+    "testnet": "https://testnet-service.lisk.com",
+    "devnet": "http://localhost:9901"
+}
+
+SOCKET_ENDPOINTS = {
+    "mainnet": "wss://service.lisk.com",
+    "testnet": "wss://testnet-service.lisk.com",
+    "devnet": "ws://host.docker.internal:8080"
+}
